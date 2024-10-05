d9c74ffc87a38866db00a9f5189feff7bf999bd8
Hiroshinoharu <maxceban2019@gmail.com>
2024-10-05 16:57:07 +0100

    Changes made to hello.py

--- hello.py
+++ hello.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p>Hello World!</p>'
+    return '<p>Hello World! <a href="/about">About</a></p>'
+
+@app.route('/about')
+def about():
+	return'<p>This is a Flask web app running in a Linux VM.</p>'
